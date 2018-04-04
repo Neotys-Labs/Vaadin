67cfe315a15c1813e06a8ce1e4615da5f2e15a7e
alexis nouvel <alexis.nouvel@neotys.com>
2018-04-04 14:20:34 +0200

    improve namer

--- src/main/java/com/neotys/codec/vaadin/VaadinNamer.java
+++ src/main/java/com/neotys/codec/vaadin/VaadinNamer.java
@@ -1,23 +1,23 @@
 package com.neotys.codec.vaadin;
 
+import com.google.common.collect.ImmutableList;
 import com.neotys.extensions.codec.functions.Namer;
 
+import java.util.List;
+
 public class VaadinNamer implements Namer {
+
+	private static final List<String> NAMES = ImmutableList.of("setText", "requestRows",
+			"select", "click", "actiontarget", "popstate", "requestChildTree", "call");
 
 	@Override
 	public String apply(final Object input) {
 		final VaadinWsRequest request = (VaadinWsRequest) input;
 		String jsonAsString = request.getContent().toString();
-		if (jsonAsString.contains("setText")) {
-			return "setText";
-		} else if (jsonAsString.contains("requestRows")) {
-			return "requestRows";
-		} else if (jsonAsString.contains("select")) {
-			return "select";
-		} else if (jsonAsString.contains("click")) {
-			return "click";
-		}
-		return "defaultName";
+		return NAMES.stream()
+				.filter(jsonAsString::contains)
+				.findFirst()
+				.orElse("defaultName");
 	}
 
 }
